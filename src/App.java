74a652a0a4720670296922ee37494fb9d387bf80
Ytalo machado <ytalomachadosantos@gmail.com>
2025-07-29 18:52:04 -0300

    comitado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("OLá, mundo!");
+        System.out.println("Bem-vindo ao Java!");
+        System.out.println("Este é um exemplo de código Java.");
+        System.out.println("Espero que você esteja se divertindo aprendendo!");
+        System.out.println("Vamos continuar explorando juntos!");
     }
 }
